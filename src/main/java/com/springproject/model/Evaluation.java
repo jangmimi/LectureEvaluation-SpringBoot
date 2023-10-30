2d590bf9406dc6387dbeaeb85a73fc3874bed640
jangmimi <alwkd0101@naver.com>
2023-10-30 21:00:13 +0900

    간격 수정

--- src/main/java/com/springproject/model/Evaluation.java
+++ src/main/java/com/springproject/model/Evaluation.java
@@ -15,20 +15,32 @@ public class Evaluation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 증가
     private Long id;
+
     @Column(length = 20)
     private Long userNumber;
+
     @Column(length = 50)
     private String lectureId;   // 강의번호
+
     @Column(length = 2048)
     private String lectureName;
+
     @Column(length = 50)
     private String lectureProfessor;
+
     @Column(length = 2048)
     private String lectureURL;
+
     @Column(length = 50)
     private String evaluationTitle;
+
     @Column(length = 2048)
     private String evaluationContent;
+
     @Column
     private int likeCount;
+
+//    @ManyToOne (fetch = FetchType.LAZY)
+//    @JoinColumn (name = "userNumber")
+//    private User user;
 }
