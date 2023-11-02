f1234b503ea0e09d2a3cc37d8d4ac44e18961cc2
jangmimi <alwkd0101@naver.com>
2023-11-02 23:08:09 +0900

    강의목록 페이징 구현중 수정필요

--- src/main/java/com/springproject/controller/LectureController.java
+++ src/main/java/com/springproject/controller/LectureController.java
@@ -4,12 +4,17 @@ import com.springproject.model.Lecture;
 import com.springproject.service.LectureService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.List;
 
 @SessionAttributes("loginUser")
@@ -21,10 +26,49 @@ public class LectureController {
     private LectureService lectureService;
 
     @RequestMapping("/lectures")
-    public String lectures(Model model, HttpSession session) throws Exception {
-        List<Lecture> lectureList = lectureService.getLectureData();
-        model.addAttribute("loginUser", session.getAttribute("loginUser"));
+    public String lectures(@PageableDefault(page = 0, size = 12, direction = Sort.Direction.DESC) Pageable pageable,
+                           @RequestParam(value = "page", defaultValue = "0") int page, Model model) throws Exception {
+        Page<Lecture> lectureList = lectureService.getLectureDataByPage(page, pageable);
+
+        int totalPages = lectureList.getTotalPages() - 1;
+        int previousPage = page - 1;
+        int nextPage = page + 1;
+        int startPage = (page - 1) / 10 * 10;
+        int endPage = Math.min(startPage + 9, totalPages - 1);
+
         model.addAttribute("lectures", lectureList);
+        model.addAttribute("previous", lectureList.hasPrevious() ? previousPage : -1);
+        model.addAttribute("next", lectureList.hasNext() ? nextPage : totalPages);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("currentPage", page);
+
+        log.info("현재 페이지 : " + page);
+        log.info("시작 페이지 : " + startPage);
+        log.info("끝 페이지 : " + endPage);
+
         return "lecture";
     }
+
+//    public String lectures(Model model,
+//                            @PageableDefault(page = 0, size = 24, direction = Sort.Direction.DESC) Pageable pageable) throws Exception {
+//        Page<Lecture> lectureList = lectureService.getLectureDataByPage(pageable);
+////        model.addAttribute("loginUser", session.getAttribute("loginUser"));
+//        model.addAttribute("lectures", lectureList);
+//        model.addAttribute("page", lectureList);
+//        model.addAttribute("previous", lectureList.hasPrevious() ? lectureList.previousPageable().getPageNumber() : -1);
+//        model.addAttribute("next", lectureList.hasNext() ? lectureList.nextPageable().getPageNumber() : lectureList.getTotalPages());
+//        model.addAttribute("startPage", lectureList.getNumber() / 10 * 10);
+//        model.addAttribute("endPage", Math.min(lectureList.getNumber() / 10 * 10 + 9, lectureList.getTotalPages() - 1));
+//
+//        log.info("전체페이지수 : " + lectureList.getTotalPages());
+//
+//        return "lecture";
+//    }
+//    public String lectures(Model model, HttpSession session) throws Exception {
+//        List<Lecture> lectureList = lectureService.getLectureData();
+//        model.addAttribute("loginUser", session.getAttribute("loginUser"));
+//        model.addAttribute("lectures", lectureList);
+//        return "lecture";
+//    }
 }
--- src/main/java/com/springproject/service/LectureService.java
+++ src/main/java/com/springproject/service/LectureService.java
@@ -6,6 +6,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -16,29 +20,88 @@ import java.util.List;
 @Slf4j
 @Service
 public class LectureService {
-    private static String lecture_URL = "https://www.inflearn.com/courses?order=seq";
+    // 크롤링 대상 URL (인프런)
+    public static final String basicURL = "https://nomadcoders.co/courses";
+//    public static final String basicURL = "https://www.inflearn.com/courses?types=ONLINE&page=";
 
-    @PostConstruct
-    public List<Lecture> getLectureData() throws IOException {
+    /**
+     * 크롤링 데이터 배열 생성
+     * */
+    public List<Lecture> crawlLectureData(String url) throws IOException {
+        // 크롤링 데이터 담는 리스트
         List<Lecture> lectureList = new ArrayList<>();
-        Document document = Jsoup.connect(lecture_URL).get();
 
-        Elements contents = document.select("div.card");
+        // Jsoup을 사용해 현재 페이지 HTML 가져옴
+        Document document = Jsoup.connect(basicURL).get();
+
+        // 원하는 데이터 선택 및 추출
+        Elements contents = document.select("div.sc-f4fbfce1-0");
 //        log.info(contents.toString());
-
         for (Element content : contents) {
+            String imageSrcset = content.select("img").attr("srcset");
+            // srcset 값을 파싱하여 이미지 URL을 추출
+            String imageUrl = parseImageSrcset(imageSrcset);
+
+            // 데이터 추출 및 처리
             Lecture lectures = Lecture.builder()
                     .productId(content.select(".card").attr("data-productid"))
-                    .image(content.select("a img").attr("abs:src"))
-                    .subject(content.select(".card-content .course_title").text())
-                    .professor(content.select(".card-content .instructor").text())
-                    .url(content.select(".card a").attr("abs:href"))
+                    .image("https://nomadcoders.co/" + imageUrl)
+                    .subject(content.select("a h3").text())
+                    .professor(content.select("a h4").text())
+                    .url(content.select(".info a").attr("abs:href"))
                     .reviewCnt(content.select(".card-content .review_cnt").text())
                     .build();
             lectureList.add(lectures);
         }
+
+//        for (Element content : contents) {
+//            // 데이터 추출 및 처리
+//            Lecture lectures = Lecture.builder()
+//                    .productId(content.select(".card").attr("data-productid"))
+//                    .image(content.select("a img").attr("abs:src"))
+//                    .subject(content.select(".card-content .course_title").text())
+//                    .professor(content.select(".card-content .instructor").text())
+//                    .url(content.select(".card a").attr("abs:href"))
+//                    .reviewCnt(content.select(".card-content .review_cnt").text())
+//                    .build();
+//            lectureList.add(lectures);
+//        }
+
 //        log.info(lectureList.toString());
         return lectureList;
     }
 
+    // srcset 값을 파싱하여 이미지 URL 추출하는 함수
+    private static String parseImageSrcset(String srcset) {
+        String[] srcsetParts = srcset.split(" ");
+        String imageUrl = srcsetParts[0]; // 첫 번째 이미지 URL 선택
+        return imageUrl;
+    }
+
+    /**
+     * 페이지네이션
+     * */
+    public Page<Lecture> getLectureDataByPage(int pageNumber, Pageable pageable) throws IOException {
+        List<Lecture> lectureList = crawlLectureData(basicURL);
+
+        int pageSize = pageable.getPageSize();
+        int startItem = pageNumber * pageSize;
+        int endItem = Math.min(startItem + pageSize, lectureList.size());
+
+        List<Lecture> pageLectures = lectureList.subList(startItem, endItem);
+
+        return new PageImpl<>(pageLectures, pageable, lectureList.size()); // pageNumber 사용
+
+//        List<Lecture> lectureList = getLectureData(pageNumber);
+//
+//        int currentPage = pageable.getPageNumber() + 1;
+//        int pageSize = pageable.getPageSize();
+//
+//        int startIndex = (currentPage - 1) * pageSize;
+//        int endIndex = Math.min(startIndex + pageSize, lectureList.size());
+//
+//        List<Lecture> pageLectures = lectureList.subList(startIndex, endIndex);
+//
+//        return new PageImpl<>(pageLectures, pageable, lectureList.size());
+    }
 }
