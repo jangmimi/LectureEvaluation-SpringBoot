491e9848529160f0a34441b67487772913298a3e
jangmimi <alwkd0101@naver.com>
2023-11-04 11:57:19 +0900

    강의 페이지 데이터 크롤링 구현

--- src/main/java/com/springproject/service/LectureService.java
+++ src/main/java/com/springproject/service/LectureService.java
@@ -8,11 +8,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,69 +18,58 @@ import java.util.List;
 @Slf4j
 @Service
 public class LectureService {
-    // 크롤링 대상 URL (인프런)
-    public static final String basicURL = "https://nomadcoders.co/courses";
-//    public static final String basicURL = "https://www.inflearn.com/courses?types=ONLINE&page=";
+    // 크롤링 대상 URL
+    public static final String BASIC_URL = "https://nomadcoders.co/courses";
+    public int productIdCounter = 0;   // productId를 위한 카운터 변수
 
-    /**
-     * 크롤링 데이터 배열 생성
-     * */
-    public List<Lecture> crawlLectureData(String url) throws IOException {
-        // 크롤링 데이터 담는 리스트
+    // 크롤링 데이터 처리
+    public List<Lecture> crawlLectureData() {
+        // 크롤링 데이터를 담을 리스트 생성
         List<Lecture> lectureList = new ArrayList<>();
 
-        // Jsoup을 사용해 현재 페이지 HTML 가져옴
-        Document document = Jsoup.connect(basicURL).get();
+        try {
+            // Jsoup을 사용해 현재 페이지 HTML 가져옴
+            Document document = Jsoup.connect(BASIC_URL).get();
 
-        // 원하는 데이터 선택 및 추출
-        Elements contents = document.select("div.sc-f4fbfce1-0");
-//        log.info(contents.toString());
-        for (Element content : contents) {
-            String imageSrcset = content.select("img").attr("srcset");
-            // srcset 값을 파싱하여 이미지 URL을 추출
-            String imageUrl = parseImageSrcset(imageSrcset);
+            // 원하는 데이터 선택 및 추출
+            Elements contents = document.select("div.sc-f4fbfce1-0");
 
-            // 데이터 추출 및 처리
-            Lecture lectures = Lecture.builder()
-                    .productId(content.select(".card").attr("data-productid"))
-                    .image("https://nomadcoders.co/" + imageUrl)
-                    .subject(content.select("a h3").text())
-                    .professor(content.select("a h4").text())
-                    .url(content.select(".info a").attr("abs:href"))
-                    .reviewCnt(content.select(".card-content .review_cnt").text())
-                    .build();
-            lectureList.add(lectures);
+            productIdCounter = 0;
+
+            for (Element content : contents) {
+                String imageSrcset = content.select("img").attr("srcset");
+                String imageUrl = parseImageSrcset(imageSrcset);
+
+                Lecture lectures = buildLectureFromElement(content, imageUrl);
+                lectureList.add(lectures);
+            }
+            log.info(lectureList.toString());
+        } catch (IOException e) {
+            log.error("데이터 크롤링 오류 발생 : " + e.getMessage());
         }
 
-//        for (Element content : contents) {
-//            // 데이터 추출 및 처리
-//            Lecture lectures = Lecture.builder()
-//                    .productId(content.select(".card").attr("data-productid"))
-//                    .image(content.select("a img").attr("abs:src"))
-//                    .subject(content.select(".card-content .course_title").text())
-//                    .professor(content.select(".card-content .instructor").text())
-//                    .url(content.select(".card a").attr("abs:href"))
-//                    .reviewCnt(content.select(".card-content .review_cnt").text())
-//                    .build();
-//            lectureList.add(lectures);
-//        }
-
-//        log.info(lectureList.toString());
         return lectureList;
+    }
+
+    private Lecture buildLectureFromElement(Element content, String imageUrl) {
+        return Lecture.builder()
+                .lectureId(++productIdCounter)  // 고유한 ID 생성
+                .lectureSubject(content.select("a h3").text())
+                .lectureInfo(content.select("a h4").text())
+                .image("https://nomadcoders.co/" + imageUrl)
+                .url(content.select(".info a").attr("abs:href"))
+                .build();
     }
 
     // srcset 값을 파싱하여 이미지 URL 추출하는 함수
     private static String parseImageSrcset(String srcset) {
         String[] srcsetParts = srcset.split(" ");
-        String imageUrl = srcsetParts[0]; // 첫 번째 이미지 URL 선택
-        return imageUrl;
+        return srcsetParts[0];     // 첫 번째 이미지 URL 선택
     }
 
-    /**
-     * 페이지네이션
-     * */
-    public Page<Lecture> getLectureDataByPage(int pageNumber, Pageable pageable) throws IOException {
-        List<Lecture> lectureList = crawlLectureData(basicURL);
+    // 페이지네이션
+    public Page<Lecture> getLectureDataByPage(int pageNumber, Pageable pageable) {
+        List<Lecture> lectureList = crawlLectureData();
 
         int pageSize = pageable.getPageSize();
         int startItem = pageNumber * pageSize;
@@ -90,18 +77,6 @@ public class LectureService {
 
         List<Lecture> pageLectures = lectureList.subList(startItem, endItem);
 
-        return new PageImpl<>(pageLectures, pageable, lectureList.size()); // pageNumber 사용
-
-//        List<Lecture> lectureList = getLectureData(pageNumber);
-//
-//        int currentPage = pageable.getPageNumber() + 1;
-//        int pageSize = pageable.getPageSize();
-//
-//        int startIndex = (currentPage - 1) * pageSize;
-//        int endIndex = Math.min(startIndex + pageSize, lectureList.size());
-//
-//        List<Lecture> pageLectures = lectureList.subList(startIndex, endIndex);
-//
-//        return new PageImpl<>(pageLectures, pageable, lectureList.size());
+        return new PageImpl<>(pageLectures, pageable, lectureList.size());
     }
 }
