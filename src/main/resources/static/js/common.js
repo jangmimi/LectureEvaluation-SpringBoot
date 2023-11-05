2f89fcf271e9aec17dc692f783fe20fb2b6d818f
jangmimi <alwkd0101@naver.com>
2023-11-05 23:24:32 +0900

    update js 분리 및 적용

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -93,26 +93,6 @@ function joinCheck() {
     });
 }
 
-// 정보수정 submit 전 체크
-function updateCheck() {
-    var userId = $('#userId').val().trim();
-    var userPw = $('#userPw').val().trim();
-    var errorMessage = '';
-
-    if(userId === '') {
-        errorMessage = '아이디를 입력해주세요.';
-    } else if(userPw === '') {
-        errorMessage = '비밀번호를 입력해주세요.';
-    }
-    if (errorMessage !== '') {
-        alert(errorMessage);
-        return false;
-    }
-
-    var confirmed = confirm('수정하시겠습니까?');
-    return confirmed;
-}
-
 function likeyCheck() {
     var confirmed = confirm('추천하시겠습니까?');
     return confirmed;
